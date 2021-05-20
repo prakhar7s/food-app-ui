7e0c80d5263ded6bde7bdf3956989b1b68af5479
prakhar7s <prakharshrivastava971@gmail.com>
2021-05-20 19:48:39 +0530

    some styles modified

--- src/components/planned-cooking/planned-cooking.component.jsx
+++ src/components/planned-cooking/planned-cooking.component.jsx
@@ -8,24 +8,22 @@ import { Link } from "react-router-dom";
 
 const PlannedCooking = () => (
   <div className="planned-cooking">
-    <div className="upper">
+    <div className="upper-part">
       <h1 className="upper-heading">Your Plan Cooking</h1>
       <a className="see-all" href="#">
         See All
       </a>
     </div>
-    <div className="planned">
-      <div className="cooking-image">
-        <img src={Food} alt="planned food" />
+
+    <img src={Food} alt="planned food" />
+
+    <div className="about-planned">
+      <div className="left">
+        <h1>Spicy Fried Rice</h1>
+        <h3>15 Minute | Easy Cooking</h3>
       </div>
-      <div className="about-planned">
-        <div className="left">
-          <h1>Spicy Fried Rice</h1>
-          <h3>15 Minute | Easy Cooking</h3>
-        </div>
-        <div className="open-button">
-          <Link to="/planned-recipe">&gt;</Link>
-        </div>
+      <div className="open-button">
+        <Link to="/planned-recipe">&gt;</Link>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/food-app-ui">
     <React.StrictMode>
       <App />
     </React.StrictMode>
